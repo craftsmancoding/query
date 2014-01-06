51ea753369f410513c2eeb2b7aeabb7ebbfae47c
Everett Griffiths <everett@fireproofsocks.com>
2014-01-06 08:42:35 -0800

    Preparing for repoman

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Config File for Repoman
+ *
+ * @return array
+ */
+return array(
+    'package_name' => 'Query',
+    'namespace' => 'query',
+    'description' => "a Snippet interface for xPDO's getCollection method.  This allows you to query 
+any MODX database collection and format the output, including pagination.  Query can be used
+as a replacement for getResources, and it offers much more flexibility because it's not 
+constrained to fetching only resources.",
+    'version' => '1.0.0',
+    'release' => 'rc',
+    'author_name' => 'Everett Griffiths',
+    'author_email' => 'everett@craftsmancoding.com',
+    'author_site' => 'Craftsman Coding',    
+    'author_url' => 'http://craftsmancoding.com/',
+    'documentation_url' => 'https://github.com/craftsmancoding/query/wiki',
+    'repo_url' => 'https://github.com/craftsmancoding/query',   
+    'clone_url' => 'git@github.com:craftsmancoding/query.git',
+    'copyright' => date('Y'),
+    
+    'category' => 'Query',
+);
+/*EOF*/
